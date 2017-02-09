e067ae54ebf3964bb3bf39502979293655f588aa
Evert Lammerts <evert@lucipher.net>
2017-02-09 13:56:08 +0100

    fixed import

--- authorization/__init__.py
+++ authorization/__init__.py
@@ -14,7 +14,7 @@ def authz_mapper(**psycopg2conf):
     This is a convenient wrapper around an :class:`authz.map.AuthzMap` for
     callers that only want to get an authorization level for a given user, even
     if that user isn't present in the authz table (in which case it will return
-    :data:`authz.levels.LEVEL_DEFAULT`).
+    :data:`authorization_levels.LEVEL_DEFAULT`).
 
     Example usage:
 
@@ -24,25 +24,14 @@ def authz_mapper(**psycopg2conf):
         mapper('username')  # => returns username's authz level
 
     :param psycopg2conf: See :function:`psycopg2.connect`
-    :return: One of LEVEL_* defined in :module:`authz.levels`
+    :return: One of LEVEL_* defined in :module:`authorization_levels`
     """
     from .map import AuthzMap
-    from . import levels
+    import authorization_levels
 
     authzmap = AuthzMap(**psycopg2conf)
 
     def getter(username):
-        return authzmap.get(username, levels.LEVEL_DEFAULT)
+        return authzmap.get(username, authorization_levels.LEVEL_DEFAULT)
 
     return getter
-
-
-def is_authorized(granted, needed):
-    """ Return True iff the `granted` authz level is sufficient to access
-    resources protected with `needed`.
-
-    :param granted: One of LEVEL_* defined in :module:`authz.levels`
-    :param needed: One of LEVEL_* defined in :module:`authz.levels`
-    :return: True if `granted` is sufficient, False otherwise
-    """
-    return needed & granted == needed
