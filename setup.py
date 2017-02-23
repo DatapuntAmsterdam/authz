cbe9148fcb9df7a54ed1d8f7517f0f7770363cda
Evert Lammerts <evert@lucipher.net>
2017-02-23 15:39:32 +0100

    version bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires_test = ['pytest>=3.0.5', 'pytest-cov>=2.4.0']
 
 setup(
     name='datapunt-authorization',
-    version='0.2.0',
+    version='0.2.1',
     description='Datapunt authorization',
     long_description=long_description,
     url='https://github.com/DatapuntAmsterdam/authorization',
