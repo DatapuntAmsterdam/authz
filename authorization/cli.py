da99304b48e552ee1558294b3d4c23b3f368a83b
Evert Lammerts <evert@lucipher.net>
2017-02-27 14:42:36 +0100

    env var names

--- authorization/cli.py
+++ authorization/cli.py
@@ -16,11 +16,11 @@ valid_levels = {
 
 @click.group()
 @click.option('--debug', is_flag=True)
-@click.option('--psql-host', default='localhost', type=str, envvar='PG_HOST')
-@click.option('--psql-port', default=5432, type=int, envvar='PG_PORT')
-@click.option('--psql-db', type=str, envvar='PG_DB')
-@click.option('--psql-user', type=str, envvar='PG_USER')
-@click.option('--psql-password', type=str, prompt=True, hide_input=True, envvar='PG_PASS')
+@click.option('--psql-host', default='localhost', type=str, envvar='DB_HOST')
+@click.option('--psql-port', default=5432, type=int, envvar='DB_PORT')
+@click.option('--psql-db', type=str, envvar='DB_DATABASE')
+@click.option('--psql-user', type=str, envvar='DB_USER')
+@click.option('--psql-password', type=str, prompt=True, hide_input=True, envvar='DB_PASS')
 @click.pass_context
 def cli(ctx, debug, psql_host, psql_port, psql_db, psql_user, psql_password):
     try:
